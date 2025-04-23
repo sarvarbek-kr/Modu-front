344fa1d591e8cd75f6f418fbbe7c2f6fd24ea164
Walter <sarvarbek200028@gmail.com>
2025-04-23 22:26:43 +0900

    feat: Trend Furniture trend card box is published like left only

--- libs/components/homepage/TrendFurnitureCard.tsx
+++ libs/components/homepage/TrendFurnitureCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Stack, Box, Divider, Typography } from '@mui/material';
+import { Stack, Box, Divider, Typography, Button } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import useDeviceDetect from '../../hooks/useDeviceDetect';
 import FavoriteIcon from '@mui/icons-material/Favorite';
@@ -61,32 +61,38 @@ const TrendFurnitureCard = (props: TrendFurnitureCardProps) => {
 	} else {
 		return (
 			<Stack className="trend-card-box" key={furniture._id}>
-				<Box
-					component={'div'}
-					className={'card-img'}
-					style={{ backgroundImage: `url(${REACT_APP_API_URL}/${furniture?.furnitureImages[0]})` }}
-					onClick={() => pushDetailHandler(furniture._id)}
-				>
-					<div className="view-like-box">
-						<IconButton color={'default'} onClick={() => likeFurnitureHandler(user, furniture?._id)}>
-							{furniture?.meLiked && furniture?.meLiked[0]?.myFavorite ? (
-								<FavoriteIcon style={{ color: 'red' }} />
-							) : (
-								<FavoriteBorderIcon />
-							)}
-						</IconButton>
-						<Typography className="view-cnt">{furniture?.furnitureLikes}</Typography>
-					</div>
-				</Box>
-				<Box component={'div'} className={'info'}>
-					<strong className={'title'} onClick={() => pushDetailHandler(furniture._id)}>
-						{furniture.furnitureTitle}
-					</strong>
-					<div className={'bott'}>
-						<div className="furnBrand">{furniture?.furnitureBrand}</div>
-						<div className="furnPrice">${furniture.furniturePrice}</div>
-					</div>
-				</Box>
+				<Stack className="container">
+					<Box
+						component={'div'}
+						className={'card-img'}
+						style={{ backgroundImage: `url(${REACT_APP_API_URL}/${furniture?.furnitureImages[0]})` }}
+						onClick={() => pushDetailHandler(furniture._id)}
+					>
+						{' '}
+						<Button className={'shop-btn'} onClick={() => pushDetailHandler(furniture._id)}>
+							<b>View Item</b>
+						</Button>
+						<div className="view-like-box">
+							<IconButton className="like" color={'default'} onClick={() => likeFurnitureHandler(user, furniture?._id)}>
+								{furniture?.meLiked && furniture?.meLiked[0]?.myFavorite ? (
+									<FavoriteIcon style={{ color: 'red' }} />
+								) : (
+									<FavoriteBorderIcon />
+								)}
+							</IconButton>
+							<Typography className="view-cnt">{furniture?.furnitureLikes}</Typography>
+						</div>
+					</Box>
+					<Box component={'div'} className={'info'}>
+						<strong className={'title'} onClick={() => pushDetailHandler(furniture._id)}>
+							{furniture.furnitureTitle}
+						</strong>
+						<div className={'bott'}>
+							<div className="furnBrand">{furniture?.furnitureBrand}</div>
+							<div className="furnPrice">${furniture.furniturePrice}</div>
+						</div>
+					</Box>
+				</Stack>
 			</Stack>
 		);
 	}
