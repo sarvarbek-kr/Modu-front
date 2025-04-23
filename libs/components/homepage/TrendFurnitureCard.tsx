aec1edeb3ce4c402b53b38ba2fc3f87458b5a7d0
Walter <sarvarbek200028@gmail.com>
2025-04-23 16:50:37 +0900

    feat: Trend Furniture box published

--- libs/components/homepage/TrendFurnitureCard.tsx
+++ libs/components/homepage/TrendFurnitureCard.tsx
@@ -3,12 +3,12 @@ import { Stack, Box, Divider, Typography } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import useDeviceDetect from '../../hooks/useDeviceDetect';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
-import { REACT_APP_API_URL } from '../../config';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { useRouter } from 'next/router';
 import { useReactiveVar } from '@apollo/client';
 import { userVar } from '../../../apollo/store';
 import { Furniture } from '../../types/furniture/furniture';
+import { REACT_APP_API_URL } from '../../config';
 
 interface TrendFurnitureCardProps {
 	furniture: Furniture;
@@ -67,23 +67,24 @@ const TrendFurnitureCard = (props: TrendFurnitureCardProps) => {
 					style={{ backgroundImage: `url(${REACT_APP_API_URL}/${furniture?.furnitureImages[0]})` }}
 					onClick={() => pushDetailHandler(furniture._id)}
 				>
-					<div>${furniture.furniturePrice}</div>
+					<div className="view-like-box">
+						<IconButton color={'default'} onClick={() => likeFurnitureHandler(user, furniture?._id)}>
+							{furniture?.meLiked && furniture?.meLiked[0]?.myFavorite ? (
+								<FavoriteIcon style={{ color: 'red' }} />
+							) : (
+								<FavoriteBorderIcon />
+							)}
+						</IconButton>
+						<Typography className="view-cnt">{furniture?.furnitureLikes}</Typography>
+					</div>
 				</Box>
 				<Box component={'div'} className={'info'}>
 					<strong className={'title'} onClick={() => pushDetailHandler(furniture._id)}>
 						{furniture.furnitureTitle}
 					</strong>
 					<div className={'bott'}>
-						<div className="view-like-box">
-							<IconButton color={'default'} onClick={() => likeFurnitureHandler(user, furniture?._id)}>
-								{furniture?.meLiked && furniture?.meLiked[0]?.myFavorite ? (
-									<FavoriteIcon style={{ color: 'red' }} />
-								) : (
-									<FavoriteIcon />
-								)}
-							</IconButton>
-							<Typography className="view-cnt">{furniture?.furnitureLikes}</Typography>
-						</div>
+						<div className="furnBrand">{furniture?.furnitureBrand}</div>
+						<div className="furnPrice">${furniture.furniturePrice}</div>
 					</div>
 				</Box>
 			</Stack>
