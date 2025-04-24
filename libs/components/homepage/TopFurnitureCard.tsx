f2907186be5262f9f40e4c74ab5f790fa6041a04
Walter <sarvarbek200028@gmail.com>
2025-04-25 03:23:20 +0900

    feat: top Furniture published

--- libs/components/homepage/TopFurnitureCard.tsx
+++ libs/components/homepage/TopFurnitureCard.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Stack, Box, Divider, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Stack, Box, Typography, Button, Tooltip, Snackbar } from '@mui/material';
 import IconButton from '@mui/material/IconButton';
 import useDeviceDetect from '../../hooks/useDeviceDetect';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShareIcon from '@mui/icons-material/Share';
 import { Furniture } from '../../types/furniture/furniture';
-import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import { REACT_APP_API_URL } from '../../config';
 import { useRouter } from 'next/router';
 import { useReactiveVar } from '@apollo/client';
@@ -20,7 +21,15 @@ const TopFurnitureCard = (props: TopFurnitureCardProps) => {
 	const device = useDeviceDetect();
 	const router = useRouter();
 	const user = useReactiveVar(userVar);
+	const [openSnackbar, setOpenSnackbar] = useState(false);
 
+	const copyLinkHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.stopPropagation(); // boshqa eventlarni to‘xtatish
+		const link = `${window.location.origin}/furniture/detail?id=${furniture._id}`;
+		navigator.clipboard.writeText(link).then(() => {
+			setOpenSnackbar(true);
+		});
+	};
 	/** HANDLERS **/
 	const pushDetailHandler = async (furnitureId: string) => {
 		console.log('furnitureId:', furnitureId);
@@ -29,38 +38,21 @@ const TopFurnitureCard = (props: TopFurnitureCardProps) => {
 
 	if (device === 'mobile') {
 		return (
-			<Stack className="top-card-box">
+			<Stack className="top-card-box" key={furniture._id}>
 				<Box
 					component={'div'}
 					className={'card-img'}
 					style={{ backgroundImage: `url(${REACT_APP_API_URL}/${furniture?.furnitureImages[0]})` }}
 					onClick={() => pushDetailHandler(furniture._id)}
 				>
-					<div>${furniture?.furniturePrice}</div>
+					<div>${furniture.furniturePrice}</div>
 				</Box>
 				<Box component={'div'} className={'info'}>
 					<strong className={'title'} onClick={() => pushDetailHandler(furniture._id)}>
-						{furniture?.furnitureTitle}
+						{furniture.furnitureTitle}
 					</strong>
-					<p className={'desc'}>{furniture?.furnitureBrand}</p>
-					<div className={'options'}>
-						<div>
-							<img src="/img/icons/bed.svg" alt="" />
-							<span>{furniture?.furnitureColor} color</span>
-						</div>
-						<div>
-							<img src="/img/icons/room.svg" alt="" />
-							<span>{furniture?.furnitureCondition} condition</span>
-						</div>
-					</div>
-					<Divider sx={{ mt: '15px', mb: '17px' }} />
 					<div className={'bott'}>
-						<p> {furniture.furnitureBarter ? 'Barter' : ''}</p>
 						<div className="view-like-box">
-							<IconButton color={'default'}>
-								<RemoveRedEyeIcon />
-							</IconButton>
-							<Typography className="view-cnt">{furniture?.furnitureViews}</Typography>
 							<IconButton color={'default'} onClick={() => likeFurnitureHandler(user, furniture?._id)}>
 								{furniture?.meLiked && furniture?.meLiked[0]?.myFavorite ? (
 									<FavoriteIcon style={{ color: 'red' }} />
@@ -77,48 +69,73 @@ const TopFurnitureCard = (props: TopFurnitureCardProps) => {
 	} else {
 		return (
 			<Stack className="top-card-box">
-				<Box
-					component={'div'}
-					className={'card-img'}
-					style={{ backgroundImage: `url(${REACT_APP_API_URL}/${furniture?.furnitureImages[0]})` }}
-					onClick={() => pushDetailHandler(furniture._id)}
-				>
-					<div>${furniture?.furniturePrice}</div>
-				</Box>
-				<Box component={'div'} className={'info'}>
-					<strong className={'title'} onClick={() => pushDetailHandler(furniture._id)}>
-						{furniture?.furnitureTitle}
-					</strong>
-					<p className={'desc'}>{furniture?.furnitureBrand}</p>
-					<div className={'options'}>
-						<div>
-							<img src="/img/icons/bed.svg" alt="" />
-							<span>{furniture?.furnitureColor} color</span>
-						</div>
-						<div>
-							<img src="/img/icons/room.svg" alt="" />
-							<span>{furniture?.furnitureCondition} Condition</span>
-						</div>
-					</div>
-					<Divider sx={{ mt: '15px', mb: '17px' }} />
-					<div className={'bott'}>
-						<p> {furniture.furnitureBarter ? 'Barter' : ''}</p>
+				<Stack className="container">
+					<Box
+						component={'div'}
+						className={'card-img'}
+						style={{ backgroundImage: `url(${REACT_APP_API_URL}/${furniture?.furnitureImages[0]})` }}
+					>
+						{' '}
+						<Button
+							className={'shop-btn'}
+							onClick={(e) => {
+								e.stopPropagation();
+								pushDetailHandler(furniture._id);
+							}}
+						>
+							<b>View Item</b>
+						</Button>
 						<div className="view-like-box">
-							<IconButton color={'default'}>
-								<RemoveRedEyeIcon />
+							<IconButton
+								className="shareIcon"
+								onClick={(e) => {
+									e.stopPropagation(); // prevent click from bubbling up
+									copyLinkHandler(e);
+								}}
+								style={{ color: 'white', marginLeft: '50px' }}
+							>
+								<Tooltip title="Copy Link">
+									<ShareIcon />
+								</Tooltip>
+								<div className="share" style={{ color: 'white', fontSize: '18px', fontWeight: 600, marginLeft: '9px' }}>
+									share
+								</div>
 							</IconButton>
-							<Typography className="view-cnt">{furniture?.furnitureViews}</Typography>
-							<IconButton color={'default'} onClick={() => likeFurnitureHandler(user, furniture?._id)}>
+							<IconButton
+								className="like"
+								color={'default'}
+								onClick={(e) => {
+									e.stopPropagation();
+									likeFurnitureHandler(user, furniture?._id);
+								}}
+							>
 								{furniture?.meLiked && furniture?.meLiked[0]?.myFavorite ? (
 									<FavoriteIcon style={{ color: 'red' }} />
 								) : (
-									<FavoriteIcon />
+									<FavoriteBorderIcon />
 								)}
 							</IconButton>
-							<Typography className="view-cnt">{furniture?.furnitureLikes}</Typography>
+							<Typography className="view-cnt">{furniture?.furnitureLikes} like</Typography>
 						</div>
-					</div>
-				</Box>
+					</Box>
+					<Box component={'div'} className={'info'}>
+						<strong className={'title'} onClick={() => pushDetailHandler(furniture._id)}>
+							{furniture.furnitureTitle}
+						</strong>
+						<div className={'bott'}>
+							<div className="furnBrand">{furniture?.furnitureBrand}</div>
+							<div className="furnPrice">${furniture.furniturePrice}</div>
+						</div>
+					</Box>
+					<Snackbar
+						open={openSnackbar}
+						autoHideDuration={2000}
+						onClose={() => setOpenSnackbar(false)}
+						message="Link copied to clipboard!"
+						anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+						className="snackbar-root"
+					/>
+				</Stack>
 			</Stack>
 		);
 	}
