703963925b423a075bb29f5475c4282eca0d9b28
Walter <sarvarbek200028@gmail.com>
2025-07-26 00:50:45 +0500

    feat: docker compose done fully

--- apps/modu-batch/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { ModuBatchModule } from './modu-batch.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(ModuBatchModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
--- apps/modu-batch/src/modu-batch.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class ModuBatchService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
